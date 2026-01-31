0f4f88bc2af61f25fb67f92ac51385748b541bce
Mohe <86363939+MoheEdeen@users.noreply.github.com>
2026-01-30 19:13:30 -0500

    submit button and css

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,9 @@
 <script setup></script>
 
 <template>
-  <div>
-    <h1 class="text">HIEL</h1>
+  <div class="d-flex justify-content-center align-items-center vh-100">
+    <b-card class="main-card text-center">
+      <b-button class="submit-btn" variant="primary">Submit</b-button>
+    </b-card>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createBootstrap } from 'bootstrap-vue-next'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
-createApp(App).mount('#app')
+createApp(App).use(createBootstrap()).mount('#app')
+
